65b010305aa6c0da4e357e0d7637f44333ba9b86
Yasir Malik <yasirrafiq786@gmail.com>
2020-08-01 16:21:29 +1000

    added axios

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,16 +1,25 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
 
 const Search = () => {
-    return (
-        <div>
-            <div className="ui form">
-                <div className="field">
-                    <label>Enter Search Term</label>
-                    <input className="input" />
-                </div>
-            </div>
+  const [term, setTerm] = useState('');
+
+  useEffect(() => {}, [term]);
+
+  return (
+    <div>
+      <div className="ui form">
+        <div className="field">
+          <label>Enter Search Term</label>
+          <input
+            value={term}
+            onChange={(e) => setTerm(e.target.value)}
+            className="input"
+          />
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default Search;
