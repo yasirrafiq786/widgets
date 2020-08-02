cd8c56d0ae0ef0b3af7d47c33fd14c1314d61110
Yasir Malik <yasirrafiq786@gmail.com>
2020-08-02 20:37:13 +1000

    added dropdown

--- src/App.js
+++ src/App.js
@@ -12,10 +12,16 @@ const items = [
   },
 ];
 
+const options = [
+  {label: 'The Color Red', value: 'red'},
+  {label: 'The Color Green', value: 'green'},
+  {label: 'The Color Blue', value: 'blue'},
+];
+
 export default () => {
   return (
     <div>
-      <Dropdown />
+      <Dropdown options = {options} />
     </div>
   );
 };
