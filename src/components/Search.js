4e8945ae2392a9cfdefebecd02956adc3203263a
Yasir Malik <yasirrafiq786@gmail.com>
2020-08-02 20:00:32 +1000

    solving error

--- src/components/Search.js
+++ src/components/Search.js
@@ -3,7 +3,18 @@ import axios from 'axios';
 
 const Search = () => {
   const [term, setTerm] = useState('programming');
+  const [debouncedTerm, setDebouncedTerm] = useState([term]);
   const [results, setResults] = useState([]);
+
+  useEffect(() => {
+    const timerId = setTimeout(() => {
+      setDebouncedTerm(term);
+    }, 1000);
+
+    return () => {
+      clearTimeout(timerId);
+    };
+  }, [term]);
 
   useEffect(() => {
     const search = async () => {
@@ -19,16 +30,20 @@ const Search = () => {
       setResults(data.query.search);
     };
 
-    const timeoutId = setTimeout(() => {
-      if (term) {
-        search();
-      }
-    }, 1500);
+    if (term && !results.length) {
+      search();
+    } else {
+      const timeoutId = setTimeout(() => {
+        if (term) {
+          search();
+        }
+      }, 1500);
 
-    return () => {
-      clearTimeout(timeoutId);
-    };
-  }, [term]);
+      return () => {
+        clearTimeout(timeoutId);
+      };
+    }
+  }, [results.length, term]);
 
   const renderedResults = results.map((result) => {
     return (
